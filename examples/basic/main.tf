c0fc717aecce1b42f15326b8c63399762405b9a6
solairerove <solairerove@gmail.com>
2019-01-02 21:48:25 +0300

    Create cidr_block variable in basic example
    Use it in both modules

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,8 +1,9 @@
 module "vpc" {
-  source    = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=master"
-  namespace = "${var.namespace}"
-  stage     = "${var.stage}"
-  name      = "${var.name}"
+  source     = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=master"
+  namespace  = "${var.namespace}"
+  stage      = "${var.stage}"
+  name       = "${var.name}"
+  cidr_block = "${var.cidr_block}"
 }
 
 module "dynamic_subnets" {
@@ -14,5 +15,5 @@ module "dynamic_subnets" {
   availability_zones = "${var.availability_zones}"
   vpc_id             = "${module.vpc.vpc_id}"
   igw_id             = "${module.vpc.igw_id}"
-  cidr_block         = "10.0.0.0/16"
+  cidr_block         = "${var.cidr_block}"
 }
--- examples/basic/variables.tf
+++ examples/basic/variables.tf
@@ -25,3 +25,9 @@ variable "availability_zones" {
   description = "Availability zones to use within region"
   default     = ["us-west-2a", "us-west-2b", "us-west-2c"]
 }
+
+variable "cidr_block" {
+  type        = "string"
+  description = "Classless Inter-Domain Routing block"
+  default     = "10.0.0.0/16"
+}
