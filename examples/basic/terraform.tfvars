5bb21fdc145e75e7f3521435d150106b6f6a8032
solairerove <solairerove@gmail.com>
2019-01-05 00:04:09 +0300

    Move default values from variables.tf to terraform.tfvars
    - set subnets nat_gateway_enabled as false
    - check on geodesic

--- examples/basic/terraform.tfvars
+++ examples/basic/terraform.tfvars
@@ -1,3 +1,11 @@
 namespace = "eg"
-name = "test"
-stage = "example"
+
+stage = "test"
+
+name = "example"
+
+region = "us-west-2"
+
+availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
+
+cidr_block = "10.0.0.0/16"
--- examples/basic/variables.tf
+++ examples/basic/variables.tf
@@ -11,23 +11,19 @@ variable "stage" {
 variable "name" {
   type        = "string"
   description = "Application or solution name (e.g. `app`)"
-  default     = "example"
 }
 
 variable "region" {
   type        = "string"
   description = "Region for VPC"
-  default     = "us-west-2"
 }
 
 variable "availability_zones" {
   type        = "list"
   description = "Availability zones to use within region"
-  default     = ["us-west-2a", "us-west-2b", "us-west-2c"]
 }
 
 variable "cidr_block" {
   type        = "string"
   description = "Classless Inter-Domain Routing block"
-  default     = "10.0.0.0/16"
 }
