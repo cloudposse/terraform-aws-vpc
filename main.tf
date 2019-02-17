0a018782d98b41a0aafd6fc4410c02c5fc0555ea
Vladislav Leveniuk <vladislav.leveniuk@mbp13pro.local>
2019-02-17 16:34:28 +0200

    assign_generated_ipv6_cidr_block to variables

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_vpc" "default" {
   enable_dns_support               = "${var.enable_dns_support}"
   enable_classiclink               = "${var.enable_classiclink}"
   enable_classiclink_dns_support   = "${var.enable_classiclink_dns_support}"
-  assign_generated_ipv6_cidr_block = true
+  assign_generated_ipv6_cidr_block = "${var.assign_generated_ipv6_cidr_block}"
   tags                             = "${module.label.tags}"
 }
 
