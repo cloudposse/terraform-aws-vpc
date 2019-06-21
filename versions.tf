b02d77d5b281c46e9fbecfe1d275e181af45ccbf
Andriy Knysh <aknysh@users.noreply.github.com>
2019-06-21 13:53:33 -0400

    Correctly pin `aws` provider (#36)

--- versions.tf
+++ versions.tf
@@ -4,6 +4,7 @@ terraform {
 
 # Pin the `aws` provider
 # https://www.terraform.io/docs/configuration/providers.html
+# Any non-beta version >= 2.0.0 and < 3.0.0, e.g. 2.X.Y
 provider "aws" {
-  version = ">= 2.12.0"
+  version = "~> 2.0"
 }
