8c054395a6dfbd8f6e5fe4d8e1c649212a97c12e
rverma-nikiai <rohit.verma@niki.ai>
2019-06-21 18:52:17 +0530

    manual fixes using intellij suggestions

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,20 +1,20 @@
 module "vpc" {
   source     = "git::https://github.com/rverma-nikiai/terraform-aws-vpc.git?ref=master"
-  namespace  = "${var.namespace}"
-  stage      = "${var.stage}"
-  name       = "${var.name}"
-  cidr_block = "${var.cidr_block}"
+  namespace  = var.namespace
+  stage      = var.stage
+  name       = var.name
+  cidr_block = var.cidr_block
 }
 
 module "dynamic_subnets" {
   source              = "git::https://github.com/rverma-nikiai/terraform-aws-dynamic-subnets.git?ref=master"
-  namespace           = "${var.namespace}"
-  stage               = "${var.stage}"
-  name                = "${var.name}"
-  region              = "${var.region}"
-  availability_zones  = "${var.availability_zones}"
-  vpc_id              = "${module.vpc.vpc_id}"
-  igw_id              = "${module.vpc.igw_id}"
-  cidr_block          = "${var.cidr_block}"
+  namespace           = var.namespace
+  stage               = var.stage
+  name                = var.name
+  region              = var.region
+  availability_zones  = var.availability_zones
+  vpc_id              = module.vpc.vpc_id
+  igw_id              = module.vpc.igw_id
+  cidr_block          = var.cidr_block
   nat_gateway_enabled = "false"
 }
