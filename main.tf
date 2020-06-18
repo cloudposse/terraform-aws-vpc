abf72a451c6df47feca95a794d3f2c23f75badfc
stephanevalk <22516002+stephanevalk@users.noreply.github.com>
2020-06-18 17:37:35 +0200

    Make use of latest terraform-null-label module (#49)

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "label" {
-  source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.14.0"
+  source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.16.0"
   namespace   = var.namespace
   name        = var.name
   stage       = var.stage
