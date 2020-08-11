939a7555b80392d1b7c91f71943a29f10b9d9edf
Pichate Ins <dearteno@gmail.com>
2020-08-11 12:55:33 +0700

    build(tf): upgrade to terraform 0.13

--- versions.tf
+++ versions.tf
@@ -3,19 +3,19 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 2.0"
+      version = "~> 3.1"
     }
     template = {
       source  = "hashicorp/template"
-      version = "~> 2.0"
+      version = "~> 2.1"
     }
     local = {
       source  = "hashicorp/local"
-      version = "~> 1.2"
+      version = "~> 1.4"
     }
     null = {
       source  = "hashicorp/null"
-      version = "~> 2.0"
+      version = "~> 2.1"
     }
   }
   required_version = ">= 0.13"
