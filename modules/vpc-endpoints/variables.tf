4a6bdd7fdbfca5ca9d06ce8a8a1b4075dde48a22
Yonatan Koren <me@yonatankoren.com>
2021-04-26 21:15:09 -0400

    Remove validation block, as it uses functions not yet available in the Terraform version being used in automated testing (Terraform 0.13).

--- modules/vpc-endpoints/variables.tf
+++ modules/vpc-endpoints/variables.tf
@@ -10,13 +10,6 @@ variable "gateway_vpc_endpoints" {
   }))
   description = "A map of Gateway VPC Endpoints to provision into the VPC. This is a map of objects with the following valid attributes: 'name' can either be one of 's3' or 'dynamodb'; 'policy' is optional and can be specified as null."
   default     = {}
-
-  validation {
-    condition = alltrue([
-      for o in var.gateway_vpc_endpoints : contains(["s3", "dynamodb"], o.name)
-    ])
-    error_message = "Only 's3' or 'dynamodb' can be specified for Gateway VPC Endpoints."
-  }
 }
 
 variable "interface_vpc_endpoints" {
