6c5b15fd012b7e2a82e184e2db7187d4153a000a
Yonatan Koren <me@yonatankoren.com>
2021-04-30 10:20:58 -0400

    Remove unnecessary vpc data resource.

--- modules/vpc-endpoints/main.tf
+++ modules/vpc-endpoints/main.tf
@@ -2,11 +2,6 @@ locals {
   enabled = module.this.enabled && (length(var.gateway_vpc_endpoints) > 0 || length(var.interface_vpc_endpoints) > 0)
 }
 
-data "aws_vpc" "vpc" {
-  count = local.enabled ? 1 : 0
-  id    = var.vpc_id
-}
-
 data "aws_vpc_endpoint_service" "gateway_endpoint_service" {
   for_each     = local.enabled ? var.gateway_vpc_endpoints : {}
   service      = var.gateway_vpc_endpoints[each.key].name
@@ -24,7 +19,7 @@ resource "aws_vpc_endpoint" "gateway_endpoint" {
   service_name      = data.aws_vpc_endpoint_service.gateway_endpoint_service[each.key].service_name
   policy            = var.gateway_vpc_endpoints[each.key].policy
   vpc_endpoint_type = data.aws_vpc_endpoint_service.gateway_endpoint_service[each.key].service_type
-  vpc_id            = data.aws_vpc.vpc[0].id
+  vpc_id            = var.vpc_id
 
   tags = merge(
     module.this.tags,
@@ -38,7 +33,7 @@ resource "aws_vpc_endpoint" "interface_endpoint" {
   security_group_ids  = var.interface_vpc_endpoints[each.key].security_group_ids
   subnet_ids          = var.interface_vpc_endpoints[each.key].subnet_ids
   vpc_endpoint_type   = data.aws_vpc_endpoint_service.interface_endpoint_service[each.key].service_type
-  vpc_id              = data.aws_vpc.vpc[0].id
+  vpc_id              = var.vpc_id
   private_dns_enabled = var.interface_vpc_endpoints[each.key].private_dns_enabled
 
   tags = merge(
