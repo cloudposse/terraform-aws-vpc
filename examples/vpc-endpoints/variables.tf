fd202404e213e38c369bb25acbd222f8c492a650
Yonatan Koren <me@yonatankoren.com>
2021-04-30 09:54:14 -0400

    Add descriptions to all variables and outputs in examples/vpc-endpoints.

--- examples/vpc-endpoints/variables.tf
+++ examples/vpc-endpoints/variables.tf
@@ -1,7 +1,9 @@
 variable "region" {
-  type = string
+  type        = string
+  description = "The AWS region where the VPC to deploy the VPC and the VPC endpoints to."
 }
 
 variable "availability_zones" {
-  type = list(string)
+  type        = list(string)
+  description = "The list of Availability Zones within the AWS region to deploy the VPC's subnets into."
 }
