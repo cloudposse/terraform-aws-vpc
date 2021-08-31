75a51044190628fa311787cc11efc4b778fa9f65
Nuru <Nuru@users.noreply.github.com>
2021-08-31 15:54:02 -0700

    Fix typo

--- variables.tf
+++ variables.tf
@@ -35,7 +35,7 @@ variable "enable_classiclink_dns_support" {
 
 variable "enable_default_security_group_with_custom_rules" {
   type        = bool
-  description = "A boolean flag to enable/disable custom and restricive inbound/outbound rules for the default VPC's SG"
+  description = "A boolean flag to enable/disable custom and restrictive inbound/outbound rules for the default VPC's SG"
   default     = true
 }
 
