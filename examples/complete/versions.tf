4f665a2f32f2c76d5b0b28990148342267bc92a2
nitrocode <nitrocode@users.noreply.github.com>
2021-11-11 13:26:56 -0600

    Update providers

--- examples/complete/versions.tf
+++ examples/complete/versions.tf
@@ -4,15 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.0"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = ">= 2.0"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = ">= 1.2"
+      version = ">= 3.0"
     }
   }
 }
