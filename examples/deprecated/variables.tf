c57625adcb081d29476ef61efd157eaf44e67c32
arnoldasbrazys <arnoldas.brazys@oag.com>
2026-09-07 11:25:45 +0300

    fix: remove deprecated examples for tests

--- examples/deprecated/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {
-  type        = string
-  description = "The region to use for the VPC"
-}
-
-variable "availability_zones" {
-  type        = list(string)
-  description = "The availability zones to use for the VPC"
-}
